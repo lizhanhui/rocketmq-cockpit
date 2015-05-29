52ba4846e78838edb079c8ba8acec75a3ab44e3c
masterasia <41795353@qq.com>
2015-05-29 11:16:54 +0800

    debug sync topic

--- cockpit-monitor/src/main/java/com/ndpmedia/rocketmq/cockpit/scheduler/command/SyncTopicCommand.java
+++ cockpit-monitor/src/main/java/com/ndpmedia/rocketmq/cockpit/scheduler/command/SyncTopicCommand.java
@@ -7,6 +7,8 @@ import com.alibaba.rocketmq.common.TopicConfig;
 import com.alibaba.rocketmq.common.protocol.body.ClusterInfo;
 import com.alibaba.rocketmq.common.protocol.body.TopicList;
 import com.alibaba.rocketmq.common.protocol.route.BrokerData;
+import com.alibaba.rocketmq.common.protocol.route.QueueData;
+import com.alibaba.rocketmq.common.protocol.route.TopicRouteData;
 import com.alibaba.rocketmq.remoting.RPCHook;
 import com.alibaba.rocketmq.remoting.exception.RemotingException;
 import com.alibaba.rocketmq.tools.admin.DefaultMQAdminExt;
@@ -15,10 +17,7 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by robert on 2015/5/29.
@@ -49,7 +48,7 @@ public class SyncTopicCommand implements SubCommand {
         options.addOption(opt);
 
         opt = new Option("g", "groupName", true, "subscription group name");
-        opt.setRequired(true);
+        opt.setRequired(false);
         options.addOption(opt);
 
         return options;
@@ -59,12 +58,12 @@ public class SyncTopicCommand implements SubCommand {
     public void execute(CommandLine commandLine, Options options, RPCHook rpcHook) {
         DefaultMQAdminExt adminExt = new DefaultMQAdminExt();
         adminExt.setInstanceName(Long.toString(System.currentTimeMillis()));
-        try{
+        try {
             adminExt.start();
             doList(adminExt);
 
             TopicList topics = adminExt.fetchAllTopicList();
-            for (String topicName:topics.getTopicList()){
+            for (String topicName : topics.getTopicList()) {
                 if (topicName.startsWith(MixAll.RETRY_GROUP_TOPIC_PREFIX) || topicName.startsWith(MixAll.DLQ_GROUP_TOPIC_PREFIX))
                     continue;
 
@@ -72,8 +71,10 @@ public class SyncTopicCommand implements SubCommand {
                 if (null != topicConfig)
                     rebuildTopicConfig(adminExt, topicConfig);
             }
-        }catch (Exception e){
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            adminExt.shutdown();
         }
     }
 
@@ -107,18 +108,46 @@ public class SyncTopicCommand implements SubCommand {
         System.out.println("[sync topic] now we get broker list , size : " + brokerList.size() + " [] " + brokerList);
     }
 
-    private TopicConfig getTopicConfig(DefaultMQAdminExt defaultMQAdminExt, String topic){
-        for (String broker:brokerList) {
-            TopicConfig topicConfig = defaultMQAdminExt.examineTopicConfig(broker, topic);
-            if (null != topicConfig && null != topicConfig.getTopicName() && !topicConfig.getTopicName().isEmpty())
-                return topicConfig;
+    private TopicConfig getTopicConfig(DefaultMQAdminExt defaultMQAdminExt, String topic) throws RemotingException, MQClientException, InterruptedException {
+        TopicConfig topicConfig = new TopicConfig();
+        topicConfig.setTopicName(topic);
+
+        TopicRouteData topicRouteData = defaultMQAdminExt.examineTopicRouteInfo(topic);
+        List<QueueData> lists = topicRouteData.getQueueDatas();
+
+        int readQ = 0;
+        int writeQ = 0;
+        int perm = 0;
+        for (QueueData queueData:lists){
+            readQ = Math.max(readQ, queueData.getReadQueueNums());
+            writeQ = Math.max(writeQ, queueData.getWriteQueueNums());
+            perm = Math.max(perm, queueData.getPerm());
         }
+        topicConfig.setWriteQueueNums(writeQ);
+        topicConfig.setReadQueueNums(readQ);
+        topicConfig.setPerm(perm);
+
+        if (null != topicConfig && null != topicConfig.getTopicName() && !topicConfig.getTopicName().isEmpty())
+            return topicConfig;
+
         System.err.println("[sync topic] big error! find topic but no topic config !");
         return null;
     }
 
     private void rebuildTopicConfig(DefaultMQAdminExt defaultMQAdminExt, TopicConfig topicConfig) throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
-        for (String broker:brokerList){
+        TopicRouteData topicRouteData = defaultMQAdminExt.examineTopicRouteInfo(topicConfig.getTopicName());
+        List<BrokerData> brokerDatas = topicRouteData.getBrokerDatas();
+        Set<String> localBroker = new HashSet<>();
+
+        for (BrokerData brokerData:brokerDatas){
+            for (Map.Entry<Long, String> entry:brokerData.getBrokerAddrs().entrySet()){
+                localBroker.add(entry.getValue());
+            }
+        }
+
+        for (String broker : brokerList) {
+            if (localBroker.contains(broker))
+                continue;
             defaultMQAdminExt.createAndUpdateTopicConfig(broker, topicConfig);
         }
     }
