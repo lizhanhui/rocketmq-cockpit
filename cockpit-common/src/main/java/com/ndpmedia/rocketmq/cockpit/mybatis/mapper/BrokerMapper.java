09e784da06dc9143c43dc647f73270225194211b
Xingwei Ruan <xingweiruan@gmail.com>
2015-06-08 17:00:19 +0800

    Add more filter

--- cockpit-common/src/main/java/com/ndpmedia/rocketmq/cockpit/mybatis/mapper/BrokerMapper.java
+++ cockpit-common/src/main/java/com/ndpmedia/rocketmq/cockpit/mybatis/mapper/BrokerMapper.java
@@ -3,6 +3,7 @@ package com.ndpmedia.rocketmq.cockpit.mybatis.mapper;
 import com.ndpmedia.rocketmq.cockpit.model.Broker;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface BrokerMapper {
@@ -13,5 +14,10 @@ public interface BrokerMapper {
 
     void refresh(Broker broker);
 
-    List<Broker> list(@Param("clusterName") String clusterName, @Param("brokerName") String brokerName, @Param("brokerId") int brokerId);
+    List<Broker> list(@Param("clusterName") String clusterName,
+                      @Param("brokerName") String brokerName,
+                      @Param("brokerId") int brokerId,
+                      @Param("dc") int dc,
+                      @Param("lastUpdateTime") Date lastUpdateTime);
+
 }
