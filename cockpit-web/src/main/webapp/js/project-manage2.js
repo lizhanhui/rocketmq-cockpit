589bdf0747a6f30afc95928936872ccbf021de10
robert xu <41795353@qq.com>
2015-06-19 15:28:33 +0800

    Create project-manage2.js

--- cockpit-web/src/main/webapp/js/project-manage2.js
+++ cockpit-web/src/main/webapp/js/project-manage2.js
@@ -15,6 +15,41 @@ $(document).ready(function() {
 
         divP.appendChild(text);
         divP.appendChild(selectP);
+
+        selectP.onchange = function() {
+            showCloud();
+            var project = $("#selectP").children('option:selected').val();
+            if (-1 != project){
+                $.ajax({
+                    async:false,
+                    url: "",//查询该project对应的Consumer Group
+                    type: "",
+                    data: project,
+                    dataType: "",
+                    contentType: "",
+                    success: function(datas){
+                        //获取结果集 插入表格
+                        //获取第一个结果，并展示其积压曲线
+                    }
+                });
+
+                $.ajax({
+                    async: false,
+                    url: "",//查询该project对应的Topic
+                    type: "",
+                    data: project,
+                    dataType: "",
+                    contentType: "",
+                    success: function(){
+                        //获取结果集 插入表格
+                        //获取第一个结果，并展示其积压曲线
+                    }
+                });
+            }else {
+                hideCloud();
+            }
+
+        };
     });
 });
 
