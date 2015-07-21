aaffb1281dc03d4c01f5b92496beb2560ac340d2
masterasia <41795353@qq.com>
2015-07-21 16:17:14 +0800

    fix:UPDATE VERSION  multi-NIC

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mvn package
-cp cockpit-web/target/cockpit-web-3.2.2.R1.war ~/jetty/webapps/
-cp doc/cockpit-web-3.2.2.R1.xml ~/jetty/webapps/
+cp cockpit-web/target/cockpit-web-3.2.2.R2.war ~/jetty/webapps/
+cp doc/cockpit-web-3.2.2.R2.xml ~/jetty/webapps/
 cp doc/start.sh ~/jetty/
