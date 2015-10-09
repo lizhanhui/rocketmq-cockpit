9d3b9a19f84839534338c2455446430d4cd574d3
Li Zhanhui <lizhanhui@gmail.com>
2015-10-09 23:08:41 +0800

    Add more status

--- cockpit-common/src/main/java/com/ndpmedia/rocketmq/cockpit/model/Status.java
+++ cockpit-common/src/main/java/com/ndpmedia/rocketmq/cockpit/model/Status.java
@@ -1,23 +1,32 @@
 package com.ndpmedia.rocketmq.cockpit.model;
 
 public enum Status {
-
-    NULL(0, "NULL"),
-    DRAFT(1, "DRAFT"),
-    ACTIVE(2, "ACTIVE"),
-    DELETED(3, "DELETED");
-
-    private String value;
+    NULL(0, "NULL", ""),
+    DRAFT(1, "DRAFT", "Draft"),
+    PENDING(2, "PENDING", "Pending"),
+    APPROVED(3, "APPROVED", "Approved"),
+    REJECTED(4, "REJECTED", "Rejected"),
+    ACTIVE(5, "ACTIVE", "Active"),
+    DELETED(6, "DELETED", "Deleted");
 
     private int id;
 
-    Status(int id, String value) {
-        this.value = value;
+    private String value;
+
+    private String text;
+
+    Status(int id, String value, String text) {
         this.id = id;
+        this.value = value;
+        this.text = text;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public String getText() {
+        return text;
     }
 
     public int getId() {
--- doc/sql/setup.sql
+++ doc/sql/setup.sql
@@ -1,8 +1,11 @@
-USE cockpit;
+USE cockpit2;
 
 INSERT INTO status_lu(id, name) VALUES (1, "DRAFT");
-INSERT INTO status_lu(id, name) VALUES (2, "ACTIVE");
-INSERT INTO status_lu(id, name) VALUES (3, "DELETED");
+INSERT INTO status_lu(id, name) VALUES (2, "PENDING");
+INSERT INTO status_lu(id, name) VALUES (3, "APPROVED");
+INSERT INTO status_lu(id, name) VALUES (4, "REJECTED");
+INSERT INTO status_lu(id, name) VALUES (5, "ACTIVE");
+INSERT INTO status_lu(id, name) VALUES (6, "DELETED");
 
 INSERT INTO cockpit_role(id, name) VALUES(1, 'ROLE_ADMIN');
 INSERT INTO cockpit_role(id, name) VALUES(2, 'ROLE_MANAGER');
